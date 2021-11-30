2aa5c45a45cda905f1f0a28bd62e89833bef574a
Sandra Sørensen <sandra99.fs@gmail.com>
2021-11-30 09:51:22 +0100

    Create funktioner_Lys.c

---
+++ funktioner_Lys.c
@@ -0,0 +1,18 @@
+/*
+ * Forlys.c
+ *
+ * Created: 30-11-2021 09:13:11
+ * Author : sandr
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
