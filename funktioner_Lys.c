139c3759d2dd4d34e5b70db04dfc5dd545da5d7c
Sandra Sørensen <sandra99.fs@gmail.com>
2021-12-01 12:58:48 +0100

    Update funktioner_Lys.c

--- funktioner_Lys.c
+++ funktioner_Lys.c
@@ -6,6 +6,21 @@
  */ 
 
 #include <avr/io.h>
+#include "Funktioner_lys.h"
+
+// Sæt alle PORTE's ben til at være udgange
+DDRE = 0xFF;
+// Clear
+PORTE = 0;
+
+// Timer 3 sættes til mode 3 (PWM 10-bit)
+// Top på 0x03FF dvs 1023
+// Clock prescaler sættes til 1 = no prescaling
+// OC3A styrer Pin B og C - Ikke fast PWM
+// clear OC3B/C when up-counting, set OC3B/C when down-counting
+
+TCCR3B = 0b10100011;  //TIL HENNING??
+TCCR3C = 0b00000001; 
 
 turnOnFrontlight(int frontLight)
 {
