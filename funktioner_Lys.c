1120e4adc014262dde1490ee2b9c8a601795a618
Sandra Sørensen <sandra99.fs@gmail.com>
2021-12-01 14:12:18 +0100

    Update funktioner_Lys.c

--- funktioner_Lys.c
+++ funktioner_Lys.c
@@ -8,17 +8,17 @@
 #include <avr/io.h>
 #include "Funktioner_lys.h"
 
-// Sæt alle PORTE's ben til at være udgange
-DDRE = 0xFF;
+//Sæt alle PORTE's ben3 til at være udgange
+DDRE |= 0b00010000;
+
 // Clear
-PORTE = 0;
+PORTE &= 0b00000000;
 
-
-//udkast
+//Udkast
 TCCR3A = 0b10000011; // Timer 3 sættes til mode 3 (PWM 10-bit) + top på 0x03FF dvs 1023 + ikke fast PWM
-					 // Clear OC3B/C when up-counting, set OC3B/C when down-counting - Ikke fast
+					 // Clear OC3B/C when up-counting, set OC3B/C when down-counting
 					 // OC3A styrer PE ben3
-TCCR3B = 0b00000001; // Clock prescaler sættes til 1 = no prescaling
+TCCR3B = 0b00000001; // Clock prescaler sættes til 1 = no prescaling, så lyset blinker så hurtigt, at vi ikke kan se det
 
 
 
