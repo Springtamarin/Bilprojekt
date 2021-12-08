9fa7ae1a3895990714864a9769d74283e5eb9ce3
MustiNejaat <95275459+MustiNejaat@users.noreply.github.com>
2021-12-08 14:28:54 +0100

    Update funktioner_Lys.c

--- funktioner_Lys.c
+++ funktioner_Lys.c
@@ -8,48 +8,35 @@
 #include <avr/io.h>
 #include "Funktioner_lys.h"
 
-//Forlys:
-
+//Forlys
 //Sæt alle PORTE's ben3 til at være udgange
 DDRE |= 0b00010000;
-
 // Clear
 PORTE &= 0b11101111;
 
-//Udkast
-TCCR3A = 0b10000011; // Timer 3 sættes til mode 3 (PWM 10-bit) + top på 0x03FF dvs 1023 + ikke fast PWM
-					 // Clear OC3A when up-counting, set OC3A when down-counting
-					 // OCR3A styrer PE ben3
-TCCR3B = 0b00000001; // Clock prescaler sættes til 1 = no prescaling, så lyset blinker så hurtigt, at vi ikke kan se det blinker
-
-//OSCR3A er sat til fuldt lys
-OCR3A = 1023;
-
 //Baglys:
-
-<<<<<<< HEAD
 //Sæt alle PORTE's ben4 til at være udgange
 DDRE |= 0b00001000;
-
 // Clear
 PORTE &= 0b11110111;
 
 //Udkast
-TCCR3A = 0b00100011; // Timer 3 sættes til mode 3 (PWM 10-bit) + top på 0x03FF dvs 1023 + ikke fast PWM
-					// Clear OC3B when up-counting, set OC3B when down-counting
-					// Vi bruger B-systemet til baglys, OCR3B styrer PE ben4
-TCCR3B = 0b00000001; // Clock prescaler sættes til 1 = From prescaler
+TCCR3A = 0b10100011; // Timer 3 sættes til mode 3 (PWM 10-bit) + top på 0x03FF dvs 1023 + ikke fast PWM
+					 // Clear OC3A when up-counting, set OC3A when down-counting
+					 // Clear OC3B when up-counting, set OC3B when down-counting
+TCCR3B = 0b00000001; // Clock prescaler sættes til 1 = no prescaling, så lyset blinker så hurtigt, at vi ikke kan se det blinker
 
-//OSCR3B er sat til fuldt lys
+//A-system til forlys. OSCR3A styrer PE ben3, og er sat til fuldt lys
+OCR3A = 1023;
+
+
+//B system til baglys. OSCR3B styrer PE ben4 er sat til fuldt lys
 OCR3B = 1023;
 
 //OSCR3B er sat til 50% duty cycle
 OCR3B = 511;
 
 
-=======
-//Ved baglys kan vi bare bruge et andet ben fx 4.
->>>>>>> 218cfea34595abed33a69affec4350f892a21dfc
 
 turnOnFrontlight(int frontLight)
 {
